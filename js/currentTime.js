0d7604fc1ea83c753354fc56a1ecbdec3114a240
Dylan Howard <DylanHoward6@gmail.com>
2022-05-19 12:12:09 -0500

    Added background image roatation

--- js/currentTime.js
+++ js/currentTime.js
@@ -1,3 +1,22 @@
+function updateImage() {
+  function getImageId(index) {
+    const imageLinks = [
+      { id: '1az2xz_fotP7AchAR6Bnv7l6_-yv3hIFb', location: ''},
+      { id: '15sdB7_U2arn3GcW077G16RiYSDCdasIP', location: ''},
+      { id: '16H-pxJV83TbOnMaXrKV7uOrQtT1mbCCk', location: ''},
+      { id: '1PI6AZPR4PehE1OaKQSKlaEEJTcPpjDJd', location: ''},
+      { id: '1i61fn9ZaIFe84nNhFARBECtBY1UBpxav', location: ''},
+      { id: '1zASJXAaoh0AjD0n539LTObEiqlZpCqkw', location: ''},
+      { id: '1tBiop5JQ_A02sele8DLjz-BZvVGIj3_u', location: ''},
+      { id: '1Fydg3qPu9ZLA7rt0Zo2qP0ZdYrCmhQ4s', location: ''},
+    ];
+
+    if (index < imageLinks.length) return imageLinks[index + 1];
+    return imageLinks[0];
+  }
+
+  document.getElementById('airtame-background').textContent = `https://drive.google.com/uc?id=${getImageId()}`;
+}
 function updateClock() {
   function getTime() {
     const [meridiem, hours, minutes] = ((today) => [
@@ -14,3 +33,5 @@ function updateClock() {
 
 updateClock();
 setInterval(() => updateClock(), 5000);
+updateImage();
+setInterval(() => updateClock(), 10000);
